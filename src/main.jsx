d05f86cf0889a5a4bfa461dc15fd7200d772bfdd
codevory <shahijahan.codes@gmail.com>
2026-03-23 18:27:18 +0530

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import ThemeContext from './components/Theme/ThemeContext.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <ThemeContext>
     <BrowserRouter>
       <App />
      <SpeedInsights />
+     <Analytics />
     </BrowserRouter>
   </ThemeContext>
 )
