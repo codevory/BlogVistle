33d07b0de671cec39303df063d3d491deb32a53f
codevory <shahijahan.codes@gmail.com>
2026-03-23 18:06:21 +0530

    resuables

---
+++ src/components/Resuables/Tooltip.jsx
@@ -0,0 +1,12 @@
+import './Tooltip.css'
+
+const Tooltip = ({ children, content }) => {
+  return (
+    <div className='tooltip-container'>
+      {children}
+      <span className='tooltip-text'> {content}</span>
+    </div>
+  )
+}
+
+export default Tooltip
