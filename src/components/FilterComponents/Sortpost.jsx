4980cbc4a82426e7d8e5033f25394c299e2fb827
codevory <shahijahan.codes@gmail.com>
2026-03-23 18:05:18 +0530

    filter Components for user posts

---
+++ src/components/FilterComponents/Sortpost.jsx
@@ -0,0 +1,17 @@
+import { useTheme } from "../Theme/ThemeContext"
+
+const Sortpost = ({ onSort }) => {
+  const { isDark } = useTheme()
+  return (
+    <div className=''>
+      <select id="sort-options" className={`h-10 md:px-2 py-1 outline-0  rounded-[20px] px-1 md:rounded-3xl font-semibold border-2 overflow-hidden text-[13px] sm:text-[15px] 
+     ${isDark ? 'bg-white text-black hover:bg-zinc-300' : 'bg-zinc-800 text-white hover:bg-zinc-900'}`}
+        defaultValue="newest" onChange={(e) => onSort(e.target.value)
+        }>
+        <option value="newest">Newest</option>
+        <option value="oldest">Oldest</option>
+      </select>
+    </div>
+  )
+}
+export default Sortpost
